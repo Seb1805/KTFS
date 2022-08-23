486edf0ad9ddc9afe01eba361027c288238bf048
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2022-08-23 10:07:14 +0200

    Added hp system

--- Assets/POLYGON weapons/Scripts/Health.cs
+++ Assets/POLYGON weapons/Scripts/Health.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int currentHealth = 3;
+    public Text healtText;
+
+    float health, maxHealt = 100;
+
+    private void Start()
+    {
+        health = maxHealt;
+    }
+
+    private void Update()
+    {
+        healtText.text = "HP: " + health + "%";
+    }
 
     public void Damage(int damageAmount)
     {
-        currentHealth -= damageAmount;
+        health -= damageAmount;
 
-        if (currentHealth <= 0)
+        if (health <= 0)
         {
             gameObject.SetActive(false);
         }
+    }
+
+    private void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, (health / maxHealt));
+        healtText.color = healthColor;
     }
 }
