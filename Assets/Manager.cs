a1bd91132175d8cb880e815e63ba8d9f7b821c38
WetDrywall <Teisvhjelm@gmail.com>
2022-09-01 09:36:47 +0200

    Clicker game
    Ikke helt færdig, skal lige lave transaktionerne mellem scenerne

---
+++ Assets/Manager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public Text ClicksTotalText;
+
+    float TotalClicks;
+
+    public void AddClicks()
+    {
+        TotalClicks++;
+        ClicksTotalText.text = TotalClicks.ToString("0"); 
+    }
+}
