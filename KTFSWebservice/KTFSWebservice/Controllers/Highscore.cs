66508da1b2f11ec2042c514768ff0a51a2d3ac0f
Frank <frankjensen156@gmail.com>
2022-09-05 13:41:46 +0200

    Webservice

---
+++ KTFSWebservice/KTFSWebservice/Controllers/Highscore.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace KTFSWebservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class highscore : ControllerBase
+    {
+        static MongoClient dbClient = new MongoClient("mongodb+srv://ktfsgameserver:ktfsfpsgame@ktfs.o5iwxxd.mongodb.net/test");
+        static IMongoDatabase database = dbClient.GetDatabase("Game");
+
+        [HttpGet(Name = "GetHighscore")]
+        public IEnumerable<PlayerScore> Get()
+        {
+            var collection = database.GetCollection<PlayerScore>("scores");
+            List<PlayerScore> result = collection.Find(Builders<PlayerScore>.Filter.Empty).ToList();
+            return result;
+        }
+
+
+
+        [HttpPost(Name = "PostHighscore")]
+        public void Post(string name, int score)
+        {
+            // convert JSON string to PlayerScore
+            //var test = JsonSerializer.Deserialize<PlayerScore>(postedData);
+
+            // Pull data from the sendt form
+
+            PlayerScore ps = new PlayerScore(score, name);
+
+            var collection = database.GetCollection<PlayerScore>("scores");
+            collection.InsertOne(ps);
+
+            //collection.InsertOne(/* PlayerScore */);
+        }
+
+
+    }
+}
+
+
---
+++ KTFSWebservice/KTFSWebservice/PlayerScore.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace KTFSWebservice
+{
+    public class PlayerScore
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public int Score { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public PlayerScore(int score, string name)
+        {
+            _id = ObjectId.GenerateNewId();
+            Score = score;
+            Name = name;
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
