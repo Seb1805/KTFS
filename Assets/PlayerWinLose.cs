ca6f86daf7fc8a62dd8b059df3c010eb69f52281
Chigeru <frankjensen156@gmail.com>
2022-09-26 11:24:46 +0200

    Start on scene change to win/lose screen

---
+++ Assets/PlayerWinLose.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWinLose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayerWon()
+    {
+
+    }
+
+    public void PlayerLose(string reason)
+    {
+
+    }
+}
