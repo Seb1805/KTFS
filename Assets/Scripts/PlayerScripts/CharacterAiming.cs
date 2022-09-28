7e3807e28c9f1b27e1feb955463fff126f422b4e
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2022-09-28 08:50:53 +0200

    added new temp player character

---
+++ Assets/Scripts/PlayerScripts/CharacterAiming.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    Camera mainCamera;
+    RaycastWeapon weapon;
+
+    public Rig aimLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yawCamera = mainCamera.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(0,yawCamera,0), turnSpeed + Time.fixedDeltaTime);
+
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+
+        }
+
+        //Only make muzzle flash when we are AIMING!
+        if(aimLayer.weight == 1)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                weapon.StartFiring();
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                weapon.StopFiring();
+            }
+        }
+
+    }
+}
